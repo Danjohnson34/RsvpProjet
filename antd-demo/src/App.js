d46a5b7a78f651a9046fb30a9de1828683378524
Daniel Johnson <Daniel.Johnson@spray.com>
2019-06-30 21:46:13 -0500

    react app

---
+++ antd-demo/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Button from 'antd/es/button';
+import './App.css';
+import { Card, Row, Form } from 'antd';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Row>
+          <Card title="RSVP">
+            <Form>
+              
+            </Form>
+          </Card>
+        </Row>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
